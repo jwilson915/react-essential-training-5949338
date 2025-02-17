c21c222f729964e6e184a8d920a00fbceb71ef4a
jwilson915 <164590100+jwilson915@users.noreply.github.com>
2025-02-17 02:46:43 +0000

    first commit

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,14 +1,16 @@
-import "./App.css";
+import {useEffect, useReducer} from 'react';
+import './App.css'
 import chef from "./images/chef.jpg";
 
-function Header({ name, year }) {
-  return (
+function Header({name, year}){
+  return(
     <header>
       <h1>{name}'s Kitchen</h1>
       <p>Copyright {year}</p>
     </header>
-  );
+  )
 }
+
 const items = [
   "Macaroni and Cheese",
   "Salmon with Potatoes",
@@ -21,40 +23,47 @@ const dishObjects = items.map((dish, i) => ({
   title: dish
 }));
 
-function Main({ dishes }) {
-  return (
+console.log(dishObjects);
+
+function Main( {dishes, openStatus, onStatus} ){
+  return(
     <>
-      <div>
-        <h2>Welcome to this beautiful restaurant!</h2>
-      </div>
-      <main>
-        <img
-          src={chef}
-          height={200}
-          alt="A photo of a smiling chef owner"
-        />
-        <ul>
-          {dishes.map((dish) => (
-            <li
-              key={dish.id}
-              style={{ listStyleType: "none" }}
-            >
-              {dish.title}
-            </li>
-          ))}
-        </ul>
-      </main>
+    <div>
+      <button onClick={() => onStatus(true)}>I want to be open</button>
+      <h2>Welcome to this beautiful resturaunt! {openStatus ? "Open" : "Closed"}</h2>
+    </div>
+    <main>
+      <img 
+      src={chef} 
+      height={200} 
+      alt="A photo of a smiling chef"/>
+    <ul>
+      {dishes.map((dish) => (
+      <li key={dish.id} style={{listStyleType: "none"}}>{dish.title}</li>
+      ))}
+    </ul>
+    </main>
     </>
-  );
+);
 }
 
 function App() {
+  
+  const [status, toggle] = useReducer((status) => !status, true);
+  
+  useEffect(() => {
+    console.log(`The resturaunt is ${status ? "open" : "closed"}.`
+    )
+  }, [status]);
+  
   return (
     <div>
-      <Header name="Alex" year={new Date().getFullYear()} />
-      <Main dishes={dishObjects} />
+      <h1>The restaurant is currently {status ? "open" : "closed" }</h1>
+      <button onClick={toggle}>{status ? "Close" : "Open"} Restaurant</button>
+      <Header name="Alex" year={new Date().getFullYear()}/>
+      <Main dishes={dishObjects} openStatus={status} onStatus={toggle}/>
     </div>
   );
 }
 
-export default App;
+export default App
--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -1,8 +1,6 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const [, , third] = ["giraffe", "zebra", "bear"];
-console.log(third);
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById('root')).render(<App />)
